dc357376ab94fd1583b6230ae1407ba1a2cd6f82
Mh-Monzil <monzil246@gmail.com>
2025-06-19 22:27:18 +0600

    add: book interface & model

---
+++ src/modules/book/book.model.ts
@@ -0,0 +1,46 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "./book.interface";
+
+const bookSchema = new Schema<IBook>({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  genre: {
+    type: String,
+    enum: [
+      "FICTION",
+      "NON_FICTION",
+      "SCIENCE",
+      "HISTORY",
+      "BIOGRAPHY",
+      "FANTASY",
+    ],
+    required: true,
+  },
+  isbn: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+  copies: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+export const Book = model<IBook>("Book", bookSchema);
