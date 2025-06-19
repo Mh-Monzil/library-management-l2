bb335c2859453ae784074fa7322189083180141d
Mh-Monzil <monzil246@gmail.com>
2025-06-19 22:51:16 +0600

    add: borrow interface & model

--- src/modules/book/book.model.ts
+++ src/modules/book/book.model.ts
@@ -35,7 +35,7 @@ const bookSchema = new Schema<IBook>({
   copies: {
     type: Number,
     required: true,
-    min: 0,
+    min: [0, "Copies must be a positive number"],
   },
   available: {
     type: Boolean,
