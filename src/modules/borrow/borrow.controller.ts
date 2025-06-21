3d1c665143f026b36c9a394aab1f7b27e85c08d1
Mh-Monzil <monzil246@gmail.com>
2025-06-21 21:43:40 +0600

    add: borrow book

--- src/modules/borrow/borrow.controller.ts
+++ src/modules/borrow/borrow.controller.ts
@@ -24,20 +24,49 @@ const borrowBook = async (req: Request, res: Response) => {
 };
 
 const borrowedBookSummary = async (req: Request, res: Response) => {
-  const result = await Borrow.aggregate([
-    {
-      $group: {
-        _id: "$book",
-        totalQuantity: { $sum: "$quantity" },
+  try {
+    const result = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
       },
-    },
-  ]);
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "book",
+        },
+      },
+      {
+        $unwind: "$book",
+      },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$book.title",
+            isbn: "$book.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
 
-  res.status(200).json({
-    success: true,
-    message: "Borrowed book summary retrieved successfully",
-    data: result,
-  });
+    res.status(200).json({
+      success: true,
+      message: "Borrowed book summary retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Failed to retrieve borrowed book summary",
+      error,
+    });
+  }
 };
 
 export const BorrowController = {
